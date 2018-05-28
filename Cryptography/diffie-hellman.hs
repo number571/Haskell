70534c49c0eba54c255813d34a2f67eddffa52a7
[#571] <31502293+Number571@users.noreply.github.com>
2018-05-29 04:30:49 +0700

    Update diffie-hellman.hs

--- Cryptography/diffie-hellman.hs
+++ Cryptography/diffie-hellman.hs
@@ -4,13 +4,13 @@ module Main where
 check :: Integer -> Integer -> Bool
 check p q = if
     | q ^ (p-1) `mod` p == 1 -> True
-    | otherwise -> False
+    | q ^ (p-1) `mod` p /= 1 -> False
 
 main :: IO()
 main = do
     if check p q 
         then print "q ^ (p-1) mod p == 1"
-        else error "q ^ (p-1) mod p != 1"
+        else error "q ^ (p-1) mod p /= 1"
     print [priv_A, priv_B]
     where
         p = 3001;   a = 41
