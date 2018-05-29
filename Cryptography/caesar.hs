3d5283377b584afdc6cbb88eb051e407770b2cc7
[#571] <31502293+Number571@users.noreply.github.com>
2018-05-30 03:16:11 +0700

    Update caesar.hs

--- Cryptography/caesar.hs
+++ Cryptography/caesar.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE MultiWayIf #-}
-
 module Main where
 
 import System.IO (hSetBuffering, stdout, BufferMode(..))
@@ -8,14 +7,14 @@ import Data.Char (chr, ord, toUpper, isAlpha, isDigit)
 encrypt :: Char -> Int -> Char
 encrypt c k = if  
     | isAlpha c -> chr $ (ord c' + k - 13) `mod` 26 + ord 'A'
-    | isDigit c -> chr $ (ord c  + k - 13) `mod` 10 + ord '0'
+    | isDigit c -> chr $ (ord c  + k - 8) `mod` 10 + ord '0'
     | otherwise -> c
     where c' = toUpper c
 
 decrypt :: Char -> Int -> Char
 decrypt c k = if  
     | isAlpha c -> chr $ (ord c' - k - 13) `mod` 26 + ord 'A'
-    | isDigit c -> chr $ (ord c  - k - 13) `mod` 10 + ord '0'
+    | isDigit c -> chr $ (ord c  - k - 8) `mod` 10 + ord '0'
     | otherwise -> c
     where c' = toUpper c
 
