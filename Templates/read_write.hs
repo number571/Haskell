ae069b349a71ae6a91d6de62ac2e3bdf2aa55c56
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-01 09:00:08 +0700

    Update read_write.hs

--- Templates/read_write.hs
+++ Templates/read_write.hs
@@ -2,6 +2,7 @@ module Main where
 
 main :: IO()
 main = do
-    writeFile path "Hello World!!"
+    writeFile path "Hello World!\n"
+    appendFile path "Hello World!"
     readFile path >>= \x -> putStrLn x
     where path = "file.txt"
