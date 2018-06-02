226d3b1035292c26077cbbd25cd85420c3cbb225
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-03 06:24:26 +0700

    Create words.hs

---
+++ Templates/words.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE MultiWayIf #-}
+module Main where
+
+drop' :: Int -> [a] -> [a]
+drop' _ [] = []
+drop' x (y:ys) = if
+    | x /= 0 -> drop' (x-1) ys
+    | otherwise -> y:ys
+
+length' :: [a] -> Int
+length' [] = 0
+length' (x:xs) = 1 + length' xs
+
+isSpace :: Char -> Bool
+isSpace x = if x == ' ' 
+    || x == '\t'
+    || x == '\n'
+    then True else False
+
+wait :: String -> String
+wait [] = []
+wait (x:xs) = if
+    | isSpace x -> []
+    | otherwise -> x : wait xs
+
+words' :: String -> [String]
+words' [] = []
+words' (x:xs) = if
+    | isSpace x -> words' xs
+    | otherwise -> wait (x:xs) : words' (drop' (length' (wait (x:xs))) xs)
+
+main :: IO()
+main = print $ words' "Hello World!" 
