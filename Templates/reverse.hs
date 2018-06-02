f2c478bf8b6e4f9620958eb1537d6ef5758dcd9d
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-02 20:52:45 +0700

    Update reverse.hs

--- Templates/reverse.hs
+++ Templates/reverse.hs
@@ -4,10 +4,10 @@ length' :: [a] -> Int
 length' [] = 0
 length' (x:xs) = 1 + length' xs
 
-index :: [Int] -> [Int]
+index :: [a] -> [Int]
 index xs = [(length' xs-1), (length' xs-2) .. 0]
 
-reverse' :: [Int] -> [Int]
+reverse' :: [a] -> [a]
 reverse' xs = [ xs !! x | x <- index xs]
 
 main :: IO()
