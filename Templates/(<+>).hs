8e28ab97056359a3104b894c27f7c93b04e61579
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-03 02:10:56 +0700

    Update (<+>).hs

--- Templates/(<+>).hs
+++ Templates/(<+>).hs
@@ -1,9 +1,9 @@
 module Main where
 
-(<+>) :: (b -> c) -> (a -> b) -> (a -> c)
-(<+>) g f = \x -> g (f x)
+(<.>) :: (b -> c) -> (a -> b) -> (a -> c)
+(<.>) g f = \x -> g (f x)
 
 func :: Int -> Int
 func x = x * x
 
-main = (print <+> func) 5
+main = (print <.> func) 5
