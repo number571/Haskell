bebaca2fba8a4bfd1211bda6667f45a29cc776b5
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-02 19:17:32 +0700

    Update zipWith.hs

--- Templates/zipWith.hs
+++ Templates/zipWith.hs
@@ -2,8 +2,7 @@ module Main where
 
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] [] = []
-zipWith' _ [_] [] = []
-zipWith' _ [] [_] = []
+zipWith' _ [_] [] = []; zipWith' _ [] [_] = []
 zipWith' f (x:xs) (y:ys) = (f x y) : zipWith' f xs ys
 
 main :: IO()
