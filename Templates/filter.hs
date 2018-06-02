5b2b419e3f832d0e8151b8bc960fb96fdb3c156c
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-02 09:39:04 +0700

    Create filter.hs

---
+++ Templates/filter.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE MultiWayIf #-}
+module Main where
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs) = if
+    | f x -> x : filter' f xs
+    | otherwise -> filter' f xs
+
+main :: IO()
+main = print (filter' (>5) [0..10])
