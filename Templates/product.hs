18a17f962c7db0186fec57290eb8677f4537e90d
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-02 20:43:05 +0700

    Create product.hs

---
+++ Templates/product.hs
@@ -0,0 +1,11 @@
+module Main where
+
+foldl1' :: (a -> a -> a) -> [a] -> a
+foldl1' _ [x] = x
+foldl1' f (x:xs) = f x (foldl1' f xs)
+
+product' :: Num a => [a] -> a
+product' x = foldl1' (*) x
+
+main :: IO()
+main = print (product' [1..6])
