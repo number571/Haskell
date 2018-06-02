af2c325b031da4d0277eb98c0c58a348da0fe092
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-03 04:40:29 +0700

    Create and.hs

---
+++ Templates/and.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE MultiWayIf #-}
+module Main where
+
+not' :: Bool -> Bool
+not' False = True
+not' True = False
+
+(&&&) :: Bool -> Bool -> Bool
+(&&&) False _ = False
+(&&&) _ False = False
+(&&&) _ _ = True
+
+and' [x] = x
+and' (x:y:zs) = if
+    | not' $ x &&& y -> False
+    | otherwise -> and' (y:zs)
+
+main :: IO()
+main = print $ and' [True, True, False, True]
