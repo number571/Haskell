3e64256dceb55d756a3f46631dd45e8a1aba34d1
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-03 03:41:39 +0700

    Create repeat.hs

---
+++ Templates/repeat.hs
@@ -0,0 +1,12 @@
+module Main where
+
+take' :: Int -> [a] -> [a]
+take' 0 _ = []
+take' _ [] = []
+take' x (y:ys) = y : take' (x-1) ys
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+main :: IO()
+main = print $ take' 5 (repeat' 'A') 
