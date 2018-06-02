6d7dc90a6eb9aa576f428f3bae3e92b5db450538
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-02 20:46:26 +0700

    Update for.hs

--- Templates/for.hs
+++ Templates/for.hs
@@ -1,11 +1,8 @@
 module Main where
 
-(|>) :: a -> (a -> b) -> b
-(|>) x f = f x
-
 for :: [Integer] -> String
 for [] = []
 for (x:xs) = show x ++ "\n" ++ for xs
 
 main :: IO()
-main = for [0..10] |> putStrLn
+main = putStrLn $ for [0..10]
