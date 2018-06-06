10e1e38421fecaa7437462a5629ae19150658fa2
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-07 03:22:38 +0700

    Create concatMap.hs

---
+++ Templates/concatMap.hs
@@ -0,0 +1,8 @@
+module Main where
+
+concatMap' :: (a -> [b]) -> [a] -> [b]
+concatMap' _ [] = []
+concatMap' f (x:xs) = (f x) ++ concatMap' f xs
+
+main :: IO()
+main = print $ concatMap' (\x -> [(x,x+2,x/2)]) [1,3,5] 
