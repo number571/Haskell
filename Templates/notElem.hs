6d2c12f98f4d415187d2ef36689762112a1b1924
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-07 04:08:37 +0700

    Create notElem.hs

---
+++ Templates/notElem.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE MultiWayIf #-}
+module Main where
+
+notElem' :: Eq a => a -> [a] -> Bool
+notElem' _ [] = True
+notElem' x (y:ys) = if
+    | x == y -> False
+    | otherwise -> notElem' x ys
+
+main = print $ notElem' 11 [1,2,3,4,5]
