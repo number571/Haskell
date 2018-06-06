22efbceb78a00e7661ae5f4cdec42609ae45176a
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-07 04:43:46 +0700

    Update scanl1.hs

--- Templates/scanl1.hs
+++ Templates/scanl1.hs
@@ -3,6 +3,6 @@ module Main where
 scanl1' :: (a -> a -> a) -> [a] -> [a]
 scanl1' _ [] = []
 scanl1' _ [x] = [x]
-scanl1' f (x:y:zs) = x : scanl1' f ((f x y):zs)
+scanl1' f (x:y:zs) = x : scanl1' f (f x y : zs)
 
 main = print $ scanl1' (+) [1,2,3,4,5]
