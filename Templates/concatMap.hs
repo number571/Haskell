c972925c9b91e8073a146f8aa46924636ae0d134
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-07 03:23:17 +0700

    Update concatMap.hs

--- Templates/concatMap.hs
+++ Templates/concatMap.hs
@@ -2,7 +2,7 @@ module Main where
 
 concatMap' :: (a -> [b]) -> [a] -> [b]
 concatMap' _ [] = []
-concatMap' f (x:xs) = (f x) ++ concatMap' f xs
+concatMap' f (x:xs) = f x ++ concatMap' f xs
 
 main :: IO()
 main = print $ concatMap' (\x -> [(x,x+2,x/2)]) [1,3,5] 
