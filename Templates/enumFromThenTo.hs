3b792fca877b97c9a0d4cc62b44f8df40f3cb679
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-07 03:30:33 +0700

    Update enumFromThenTo.hs

--- Templates/enumFromThenTo.hs
+++ Templates/enumFromThenTo.hs
@@ -1,10 +1,5 @@
 module Main where
 
-take' :: Int -> [a] -> [a]
-take' 0 _ = []
-take' _ [] = []
-take' x (y:ys) = y : take' (x-1) ys
-
 enumFromThenTo' :: Enum a => a -> a -> a -> [a]
 enumFromThenTo' x y z = [x,y..z]
 
