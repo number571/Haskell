c847a1f425d8afd7d4ddccd74459e65179a3c3bf
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-09 17:02:19 +0700

    Create split.hs

---
+++ Templates/split.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE MultiWayIf #-}
+module Main where
+
+drop' :: Int -> [a] -> [a]
+drop' _ [] = []
+drop' x (y:ys) = if
+    | x /= 0 -> drop' (x-1) ys
+    | otherwise -> y:ys
+
+length' :: [a] -> Int
+length' [] = 0
+length' (x:xs) = 1 + length' xs
+
+accumulate :: Char -> String -> String
+accumulate _ [] = []
+accumulate c (x:xs) = if
+    | x == c -> []
+    | otherwise -> x : accumulate c xs
+
+split :: Char -> String -> [String]
+split _ [] = []
+split c (x:xs) = if
+    | x == c -> split c xs
+    | otherwise -> accumulate c (x:xs) : split
+        c (drop' (length' (accumulate c (x:xs))) xs)
+
+main :: IO()
+main = print $ split ' ' "Hello World!"
