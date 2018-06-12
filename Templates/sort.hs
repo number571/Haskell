74ac63d1f040964445d7ecc9f72f9db3e1b8f460
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-12 21:36:56 +0700

    Update sort.hs

--- Templates/sort.hs
+++ Templates/sort.hs
@@ -1,23 +1,23 @@
 {-# LANGUAGE MultiWayIf #-}
 module Main where
 
+length' :: [a] -> Int
+length' [] = 0
+length' (x:xs) = 1 + length' xs
+
 sort' :: Ord a => [a] -> [a]
 sort' xs = while' (length' xs) sorting' xs
    where
-      sorting' :: Ord a => [a] -> [a]
-      sorting' [] = []
-      sorting' [x] = [x]
-      sorting' (x:y:xs) = if
-         | x > y -> y:x:sorting' xs
-         | otherwise -> x : sorting' (y:xs)
-
       while' :: Ord a => Int -> ([a] -> [a]) -> [a] -> [a]
       while' 0 f xs = f xs
       while' n f xs = while' (n-1) f (f xs)
 
-length' :: [a] -> Int
-length' [] = 0
-length' (x:xs) = 1 + length' xs
+      sorting' :: Ord a => [a] -> [a]
+      sorting' [] = []
+      sorting' [x] = [x]
+      sorting' (x:y:xs) = if
+         | x > y -> y : x : sorting' xs
+         | otherwise -> x : sorting' (y:xs)
 
 main :: IO()
 main = print $ sort' [8,7,9,4,5,6,3,1,2]
