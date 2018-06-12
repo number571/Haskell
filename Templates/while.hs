63df52821e5d4b61aed8c67f1360e4264f7754ab
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-12 21:46:02 +0700

    Create while.hs

---
+++ Templates/while.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE MultiWayIf #-}
+module Main where
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+while' :: Ord a => Int -> ([a] -> [a]) -> [a] -> [a]
+while' 0 f xs = xs
+while' n f xs = while' (n-1) f (f xs)
+
+main :: IO()
+main = print $ while' 5 (map' (*2)) [1,2,3,4,5]
