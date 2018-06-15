829667d03482666aee8209ace1726e89a024ffa0
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-15 07:06:31 +0700

    Create qsort.hs

---
+++ Templates/qsort.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE MultiWayIf #-}
+module Main where
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs) = if
+    | f x -> x : filter' f xs
+    | otherwise -> filter' f xs
+
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort (filter' (< x) xs) ++ [x] ++ qsort (filter' (>= x) xs)
+
+main :: IO()
+main = print $ qsort [8,7,9,0,6,3,4,5,1,2]
