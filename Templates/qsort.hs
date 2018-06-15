dc1c783a55a429befe8384991403d7e373b77d4f
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-15 07:21:17 +0700

    Update qsort.hs

--- Templates/qsort.hs
+++ Templates/qsort.hs
@@ -11,5 +11,11 @@ qsort :: Ord a => [a] -> [a]
 qsort [] = []
 qsort (x:xs) = qsort (filter' (< x) xs) ++ [x] ++ qsort (filter' (>= x) xs)
 
+{-
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x : xs) = qsort [y | y <- xs, y < x] ++ [x] ++ qsort [y | y <- xs, y >= x]
+-}
+
 main :: IO()
 main = print $ qsort [8,7,9,0,6,3,4,5,1,2]
