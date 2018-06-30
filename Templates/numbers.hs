6ceb75c436c2c89f9c51f39530189e05fb721a09
[#571] <31502293+Number571@users.noreply.github.com>
2018-06-30 16:33:08 +0700

    Update numbers.hs

--- Templates/numbers.hs
+++ Templates/numbers.hs
@@ -1,7 +1,11 @@
 module Main where
 
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
 numbers :: [Integer]
-numbers = 0 : map (+1) numbers
+numbers = 0 : map' (+1) numbers
 
 take' :: Int -> [a] -> [a]
 take' 0 _ = []
