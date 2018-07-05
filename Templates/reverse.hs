2232d4ac3106d4fd3d5120318d2e6e1cff37dedb
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-05 23:40:36 +0700

    Update reverse.hs

--- Templates/reverse.hs
+++ Templates/reverse.hs
@@ -1,19 +1,8 @@
 module Main where
 
-length' :: [a] -> Int
-length' [] = 0
-length' (x:xs) = 1 + length' xs
-
-init' :: [a] -> [a]
-init' [x] = []
-init' (x:xs) = x : init' xs
-
-last' :: [a] -> a
-last' x = x !! (length' x-1)
-
 reverse' :: [a] -> [a]
 reverse' [] = []
-reverse' xs = last' xs : reverse' (init' xs)
+reverse' (x:xs) = reverse xs ++ [x]
 
 main :: IO()
-main = print $ reverse' [0,2..10]
+main = print $ reverse' [1..5]
