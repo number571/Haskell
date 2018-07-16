03c65d7040fd443933bb0aab1eb6fa1326c068ff
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-16 22:11:45 +0700

    Update length.hs

--- Templates/length.hs
+++ Templates/length.hs
@@ -4,5 +4,10 @@ length' :: [a] -> Int
 length' [] = 0
 length' (_:xs) = 1 + length' xs
 
+{-
+length' :: [a] -> Int
+length' xs = sum [1 | _ <- xs]
+-}
+
 main :: IO()
 main = print $ length' [1..100]
