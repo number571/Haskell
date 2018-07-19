44d0eb074eabc8ce454c05ca392dfe085dd0db09
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 02:47:09 +0700

    Update any.hs

--- Templates/any.hs
+++ Templates/any.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 any' :: (a -> Bool) -> [a] -> Bool
 any' _ [] = False
-any' f (x:xs) = if
-    | not $ f x -> any' f xs
-    | otherwise -> True
+any' f (x:xs)
+    | not $ f x = any' f xs
+    | otherwise = True
 
+main :: IO()
 main = print $ any' (== 5) [1,2,3,4,5]
