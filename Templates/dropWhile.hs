3e78f6cb2e5e52bfff39d71ebf683b3d644a6183
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 02:53:09 +0700

    Update dropWhile.hs

--- Templates/dropWhile.hs
+++ Templates/dropWhile.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 dropWhile' :: (a -> Bool) -> [a] -> [a]
 dropWhile' _ [] = []
-dropWhile' f (x:xs) = if
-    | f x -> dropWhile' f xs
-    | otherwise -> x:xs
+dropWhile' f (x:xs)
+    | f x = dropWhile' f xs
+    | otherwise = x:xs
 
 main :: IO()
 main = print $ dropWhile' (< 4) [1..10]
