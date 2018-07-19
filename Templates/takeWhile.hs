59364ec14d0c8c86a374117f30fcf959c1145119
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 03:12:44 +0700

    Update takeWhile.hs

--- Templates/takeWhile.hs
+++ Templates/takeWhile.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 takeWhile' :: (a -> Bool) -> [a] -> [a]
 takeWhile' _ [] = []
-takeWhile' f (x:xs) = if
-    | f x -> x : takeWhile' f xs
-    | otherwise -> []
+takeWhile' f (x:xs)
+    | f x = x : takeWhile' f xs
+    | otherwise = []
 
 main :: IO()
 main = print $ takeWhile (<5) [1..10]
