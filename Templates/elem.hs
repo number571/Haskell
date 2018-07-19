cea69c16a90de36ff5bea1f334621249e8f4e3aa
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 02:53:47 +0700

    Update elem.hs

--- Templates/elem.hs
+++ Templates/elem.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 elem' :: Eq a => a -> [a] -> Bool
 elem' _ [] = False
-elem' x (y:ys) = if
-    | x == y -> True
-    | otherwise -> elem' x ys
+elem' x (y:ys)
+    | x == y = True
+    | otherwise = elem' x ys
 
+main :: IO()
 main = print $ elem' 3 [1,2,3,4,5]
