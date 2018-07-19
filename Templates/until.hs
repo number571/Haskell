be0133aa77ccca96e1abb9e273e6619788e96ccd
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 03:13:20 +0700

    Update until.hs

--- Templates/until.hs
+++ Templates/until.hs
@@ -1,10 +1,9 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 until' :: (a -> Bool) -> (a -> a) -> a -> a
-until' f g x = if
-    | f (g x) -> x
-    | otherwise -> until f g $ g x
+until' f g x 
+    | f (g x) = x
+    | otherwise = until f g $ g x
 
 main :: IO()
 main = print $ until' (>100) (*2) 1
