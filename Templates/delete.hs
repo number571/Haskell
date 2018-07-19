fddb6ffd03be163d83cc1ba7f9344ce983c66113
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 02:50:44 +0700

    Update delete.hs

--- Templates/delete.hs
+++ Templates/delete.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 delete' :: Eq a => a -> [a] -> [a]
 delete' _ [] = []
-delete' n (x:xs) = if
-   | n /= x -> x : delete' n xs
-   | otherwise -> xs
+delete' n (x:xs) 
+   | n /= x = x : delete' n xs
+   | otherwise = xs
 
 main :: IO()
 main = print $ delete' 9 [8,7,9,4,5,6,3,1,2]
