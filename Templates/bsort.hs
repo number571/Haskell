9b02d2525202e281f2103c4688fd4df4b3962b0d
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 02:48:45 +0700

    Update bsort.hs

--- Templates/bsort.hs
+++ Templates/bsort.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 length' :: [a] -> Int
@@ -15,9 +14,9 @@ bsort xs = while' (1 + length' xs) sorting' xs
       sorting' :: Ord a => [a] -> [a]
       sorting' [] = []
       sorting' [x] = [x]
-      sorting' (x:y:xs) = if
-         | x > y -> y : x : sorting' xs
-         | otherwise -> x : sorting' (y:xs)
+      sorting' (x:y:xs)
+         | x > y = y : x : sorting' xs
+         | otherwise = x : sorting' (y:xs)
 
 main :: IO()
 main = print $ bsort [8,7,9,4,5,6,3,1,2]
