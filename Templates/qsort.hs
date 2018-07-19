7fdebb5668726ad5514bb441742fde158dea06a9
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 03:04:34 +0700

    Update qsort.hs

--- Templates/qsort.hs
+++ Templates/qsort.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' _ [] = []
-filter' f (x:xs) = if
-    | f x -> x : filter' f xs
-    | otherwise -> filter' f xs
+filter' f (x:xs)
+    | f x = x : filter' f xs
+    | otherwise = filter' f xs
 
 qsort :: Ord a => [a] -> [a]
 qsort [] = []
