d7aaa05975d9ac84c7374675c7a1851f17a37a09
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 02:44:26 +0700

    Update all.hs

--- Templates/all.hs
+++ Templates/all.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 all' :: (a -> Bool) -> [a] -> Bool
 all' _ [] = True
-all' f (x:xs) = if
-    | f x -> all' f xs
-    | otherwise -> False
+all' f (x:xs)
+    | f x = all' f xs
+    | otherwise = False
 
+main :: IO()
 main = print $ all' (<10) [1,2,3,4,5]
