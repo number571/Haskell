71a10500696d72cc3d289623c9391c200bdcbdc2
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 02:51:27 +0700

    Update deleteBy.hs

--- Templates/deleteBy.hs
+++ Templates/deleteBy.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 deleteBy' :: (a -> a -> Bool) -> a -> [a] -> [a]
 deleteBy' _ _ [] = []
-deleteBy' f n (x:xs) = if
-   | f n x -> xs
-   | otherwise -> x : deleteBy' f n xs
+deleteBy' f n (x:xs)
+   | f n x = xs
+   | otherwise = x : deleteBy' f n xs
 
 main :: IO()
 main = print $ deleteBy' (>) 8 [8,7,9,4,5,6,3,1,2]
