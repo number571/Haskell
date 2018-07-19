eb71170a8bb3744258d0dbd9d614f6886da79872
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 03:05:02 +0700

    Update random.hs

--- Templates/random.hs
+++ Templates/random.hs
@@ -1,13 +1,13 @@
 module Main where
 import System.Random
 
-main :: IO()
-main = do
-    print =<< drawInt 1 100
-    print =<< drawDouble 1.0 100.0
-
 drawInt :: Int -> Int -> IO Int
 drawInt x y = getStdRandom . randomR $ (x,y)
 
 drawDouble :: Double -> Double -> IO Double
 drawDouble x y = getStdRandom . randomR $ (x,y)
+
+main :: IO()
+main = do
+    print =<< drawInt 1 100
+    print =<< drawDouble 1.0 100.0
