f8d37e8a6fe4e4d3627311856c47fe7d766a6c9c
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 02:43:26 +0700

    Update find.hs

--- Templates/find.hs
+++ Templates/find.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
-find' :: (a -> Bool) -> [a] -> Maybe a
-find' _ [] = Nothing
-find' f (x:xs) = if
-    | f x -> Just x
-    | otherwise -> find' f xs
+find :: (a -> Bool) -> [a] -> Maybe a
+find _ [] = Nothing
+find f (x:xs)
+    | f x = Just x
+    | otherwise = find f xs
 
 main :: IO()
-main = print $ find' (>3) [1..10]
+main = print $ find (>3) [1..10]
