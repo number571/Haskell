056eae28da15d1b0d571a6e31bc242fd0875ab61
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 03:00:12 +0700

    Update lookup.hs

--- Templates/lookup.hs
+++ Templates/lookup.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 lookup' :: Eq a => a -> [(a,b)] -> Maybe b
 lookup' _ [] = Nothing
-lookup' x ((y,z):as) = if
-    | x == y -> Just z
-    | otherwise -> lookup' x as
+lookup' x ((y,z):as)
+    | x == y = Just z
+    | otherwise = lookup' x as
 
 main :: IO()
 main = print $ lookup' 'c' [('a',1),('b',2),('c',3),('d',4)]
