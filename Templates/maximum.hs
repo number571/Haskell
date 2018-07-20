fe678d4f4753bdcb2803d6c319bd4c33364ea64c
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 22:52:07 +0700

    Update maximum.hs

--- Templates/maximum.hs
+++ Templates/maximum.hs
@@ -4,8 +4,9 @@ max' :: Ord a => a -> a -> a
 max' x y | x > y = x | otherwise = y
 
 maximum' :: Ord a => [a] -> a
+maximum' [] = undefined 
 maximum' [x] = x
-maximum' (x:y:xs) = maximum' ((max' x y):xs)
+maximum' (x:xs) = max' x $ maximum' xs
 
 main :: IO()
 main = print $ maximum' [1,5,3,8,2,3,4]
