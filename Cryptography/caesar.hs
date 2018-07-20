ac99748d6614fbcc59dcbfe761d4946327c55bfd
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-20 21:44:09 +0700

    Update caesar.hs

--- Cryptography/caesar.hs
+++ Cryptography/caesar.hs
@@ -1,33 +1,23 @@
 {-# LANGUAGE MultiWayIf #-}
-module Main where
 
-import System.IO (hSetBuffering, stdout, BufferMode(..))
+module Main where
 import Data.Char (chr, ord, toUpper, isAlpha, isDigit)
 
-encrypt :: Char -> Int -> Char
-encrypt c k = if  
-    | isAlpha c -> chr $ (ord c' + k - 13) `mod` 26 + ord 'A'
-    | isDigit c -> chr $ (ord c  + k -  8) `mod` 10 + ord '0'
-    | otherwise -> c
-    where c' = toUpper c
-
-decrypt :: Char -> Int -> Char
-decrypt c k = if  
-    | isAlpha c -> chr $ (ord c' - k - 13) `mod` 26 + ord 'A'
-    | isDigit c -> chr $ (ord c  - k -  8) `mod` 10 + ord '0'
+encrypt :: (Int -> Int -> Int) -> Char -> Int -> Char
+encrypt m c k = if  
+    | isAlpha c -> chr $ (ord c' `m` k - 65) `mod` 26 + ord 'A'
+    | isDigit c -> chr $ (ord c  `m` k - 48) `mod` 10 + ord '0'
     | otherwise -> c
     where c' = toUpper c
 
 caesar :: Char -> Int -> String -> String
 caesar m k s = if
-    | m == 'e' || m == 'E' -> [encrypt c k | c <- s]
-    | m == 'd' || m == 'D' -> [decrypt c k | c <- s]
+    | m == 'e' || m == 'E' -> [encrypt (+) c k | c <- s]
+    | m == 'd' || m == 'D' -> [encrypt (-) c k | c <- s]
     | otherwise -> "Mode is not found"
 
 main :: IO()
-main = do
-    hSetBuffering stdout NoBuffering
-    putStr "Mode: "   ; mode    <- getChar; getChar
-    putStr "Key: "    ; key     <- readLn :: IO Int
-    putStr "Message: "; message <- getLine
-    putStrLn $ "Final: " ++ caesar mode key message
+main = print [encrypted, decrypted]
+    where 
+        encrypted = caesar 'e' 3 "hello, world"
+        decrypted = caesar 'd' 3 encrypted
