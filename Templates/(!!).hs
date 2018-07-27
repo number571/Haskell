5f1f1265434b1a618961b107e9013ae579718ef7
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-28 00:17:36 +0700

    Update (!!).hs

--- Templates/(!!).hs
+++ Templates/(!!).hs
@@ -1,8 +1,8 @@
 module Main where
 
 (!!!) :: [a] -> Int -> a
-(!!!) (x:xs) 0 = x
-(!!!) (x:xs) n = (!!!) xs (n-1)
+(!!!) (x:_) 0 = x
+(!!!) (_:xs) n = (!!!) xs (n-1)
 
 main :: IO()
-main = print $ [1..10] !!! 2
+main = print $ "hello, world" !! 4
