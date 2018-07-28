339370df7e076a233c9416547b01e6f1f764763a
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-29 02:17:57 +0700

    Update (!!).hs

--- Templates/(!!).hs
+++ Templates/(!!).hs
@@ -1,8 +1,9 @@
 module Main where
 
 (!!!) :: [a] -> Int -> a
+(!!!) [] _ = undefined
 (!!!) (x:_) 0 = x
 (!!!) (_:xs) n = (!!!) xs (n-1)
 
 main :: IO()
-main = print $ "hello, world" !!! 4
+main = print $ "123" !!! 4
