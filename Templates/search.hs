38185c65ae48be1025c92460e2801f8a0dfff2a3
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-29 03:57:36 +0700

    Update search.hs

--- Templates/search.hs
+++ Templates/search.hs
@@ -1,12 +1,12 @@
 module Main where
 
-search :: Eq a => a -> [a] -> Maybe Int
+search :: Eq a => a -> [a] -> Int
 search = \y xs -> count y xs 0
     where
-        count :: Eq a => a -> [a] -> Int -> Maybe Int
-        count _ [] _ = Nothing
+        count :: Eq a => a -> [a] -> Int -> Int
+        count _ [] _ = -1
         count y (x:xs) i
-            | y == x = Just i
+            | y == x = i
             | otherwise = count y xs $ i + 1
 
 main :: IO()
