d663e403505ebba80fd64040e809611e073368f8
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-28 13:14:03 +0700

    Update concat.hs

--- Templates/concat.hs
+++ Templates/concat.hs
@@ -2,7 +2,7 @@ module Main where
 
 concat' :: [[a]] -> [a]
 concat' [] = []
-concat' (xs:ys) = xs ++ concat' ys
+concat' (xs:xss) = xs ++ concat' xss
 
 main :: IO()
 main = print $ concat' [[1,2,3],[4,5,6],[7,8,9]]
