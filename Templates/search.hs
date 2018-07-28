e61637e23b98ca23ceaa01d20ec407ce69938283
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-29 03:47:06 +0700

    Update search.hs

--- Templates/search.hs
+++ Templates/search.hs
@@ -1,12 +1,13 @@
 module Main where
 
-search :: Eq a => a -> [a] -> Int
+search :: Eq a => a -> [a] -> Maybe Int
 search = \n xs -> count n xs 0
     where
-        count :: Eq a => a -> [a] -> Int -> Int
+        count :: Eq a => a -> [a] -> Int -> Maybe Int
+        count _ [] _ = Nothing
         count n (x:xs) i
-            | n == x = i
-            | otherwise = count n xs $ i+1
+            | n == x = Just i
+            | otherwise = count n xs $ i + 1
 
 main :: IO()
 main = print $ search 'k' ['a'..'z']
