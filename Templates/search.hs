2036380aa2f205b5ead2c392eab5bd4a33b1afbf
[#571] <31502293+Number571@users.noreply.github.com>
2018-07-30 01:03:15 +0700

    Update search.hs

--- Templates/search.hs
+++ Templates/search.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE MultiWayIf #-}
 module Main where
 
+search :: Eq a => a -> [a] -> Int
+search _ [] = -1
+search y (x:xs) = if
+    | y == x -> 0
+    | otherwise -> 1 + search y xs
+
+{-
 search :: Eq a => a -> [a] -> Int
 search = \y xs -> count y xs 0
     where
         count :: Eq a => a -> [a] -> Int -> Int
         count _ [] _ = -1
-        count y (x:xs) i
-            | y == x = i
-            | otherwise = count y xs $ i + 1
+        count y (x:xs) i = if
+            | y == x -> i
+            | otherwise -> count y xs $ i + 1
+-}
 
 main :: IO()
 main = print $ search 'k' ['a'..'z']
