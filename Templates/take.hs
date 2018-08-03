8a51529804853420f3e64193ad6ee1eb949be0fb
[#571] <31502293+Number571@users.noreply.github.com>
2018-08-03 10:46:50 +0700

    Update take.hs

--- Templates/take.hs
+++ Templates/take.hs
@@ -1,9 +1,9 @@
 module Main where
 
 take' :: Int -> [a] -> [a]
-take' x _ | x <= 0 = []
+take' n _ | n <= 0 = []
 take' _ [] = []
-take' x (y:ys) = y : take' (x-1) ys
+take' n (x:xs) = x : take' (n-1) xs
 
 main :: IO()
 main = print $ take' 5 [1..10]
